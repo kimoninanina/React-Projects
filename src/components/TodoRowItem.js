3db68fa0aa74507ed0560251391f4b0786cdda32
kimoninanina <nina02929@gmail.com>
2023-10-03 16:33:49 -0700

    first commit

---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsTrash } from "react-icons/bs"; // 使用 React-icons 删除图标
+
+function TodoRowItem(props) {
+  const handleDeleteClick = (event) => {
+    event.stopPropagation(); 
+    props.deleteTodo(props.rowNumber);
+  };
+
+  return (
+    <tr>
+      <th scope="row">{props.rowNumber}</th>
+      <td>{props.rowDescription}</td>
+      <td>{props.rowAssigned}</td>
+      <td onClick={handleDeleteClick}><BsTrash /></td> 
+    </tr>
+  );
+}
+
+export default TodoRowItem;
